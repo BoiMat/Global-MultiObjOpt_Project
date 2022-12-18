4270472cf4ad482ce4f3ce67418bbd5f884dc7d9
BoiMat <boi.matteo95@gmail.com>
2022-12-18 19:03:22 +0100

    .

---
+++ Script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -q dssc
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=03:00:00
+#PBS -N serial
+
+cd $PBS_O_WORKDIR/
+
+module load conda/4.9.2
+conda activate Global
+
+python test.py
