b0664aad6e3a2398fd53eda97ed59cd4b3511bc5
BoiMat <boi.matteo95@gmail.com>
2022-12-20 10:19:35 +0100

    last commit

--- test.py
+++ test.py
@@ -20,12 +20,12 @@ def main(dataset_func = BTC_1d_Dataset, load=False, save=False):
         with open('gp_model.pkl', 'rb') as f:
             gp = pickle.load(f)
     else:
-        gp = SymbolicMaximizer(population_size=300, generations=300,
+        gp = SymbolicMaximizer(population_size=200, generations=200,
                             tournament_size=20, init_depth=(2, 6), 
                             function_set=function_set,
                             parsimony_coefficient=0.01, p_hoist_mutation=0.05, 
                             feature_names=features, 
-                            n_jobs=-1, verbose=1, random_state=42)
+                            n_jobs=-1, verbose=0, random_state=42)
 
     gp.fit(dataset, 100)
     
@@ -46,7 +46,7 @@ def main(dataset_func = BTC_1d_Dataset, load=False, save=False):
     plt.scatter(df_normalized.index[gp._program.buy_ops], df_normalized['Close'][gp._program.buy_ops], color='green', label='Buy', marker='^', alpha=1)
     # plot a sell signal
     plt.scatter(df_normalized.index[gp._program.sell_ops], df_normalized['Close'][gp._program.sell_ops], color='red', label='Sell', marker='v', alpha=1)
-    plt.savefig('Close_SP500_300p_300g.png')
+    plt.savefig('Close_SP500_200p_200g.png')
     #plt.show()
     
     
