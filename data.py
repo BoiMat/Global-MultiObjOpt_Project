17a86f2f9d920321993df1163baf144514c7a55d
BoiMat <boi.matteo95@gmail.com>
2023-01-03 16:46:46 +0100

    .

--- data.py
+++ data.py
@@ -7,8 +7,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 def BTC_1d_Dataset(zscore = False):
     
-    df = pd.read_csv('Binance_BTCUSDT_d.csv')
-    df.drop(['symbol', 'unix', 'Volume USDT', 'tradecount'], axis=1, inplace=True)
+    df = pd.read_csv('Bitstamp_BTCUSD_d.csv')
+    df.drop(['symbol', 'unix', 'Volume USD'], axis=1, inplace=True)
     df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
     df = df.sort_values('Date')
     df = AddIndicators(df)
