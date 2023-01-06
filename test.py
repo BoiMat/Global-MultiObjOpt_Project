3c1982d4acf5e9305333703ebebbe5c64fc2de0a
BoiMat <boi.matteo95@gmail.com>
2023-01-06 17:09:13 +0100

    .

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ import pickle
 
 def main(dataset_func = BTC_1d_Dataset, load=False, save=False):
     
-    name = 'Close_lowfeatures_BTC_300p_200g_newdata'
+    name = 'zscore_lowfeatures_BTC_300p_200g_newdata'
     path = 'models/' + name + '.pkl'
 
-    df, df_normalized = dataset_func(zscore=False)
+    df, df_normalized = dataset_func(zscore=True)
 
     features = df_normalized.columns[:-1]
 
