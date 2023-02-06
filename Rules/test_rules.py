15eea57e582bfa199d53f80e659ba7742e046df2
BoiMat <boi.matteo95@gmail.com>
2023-02-06 12:19:55 +0100

    .

--- Rules/test_rules.py
+++ Rules/test_rules.py
@@ -12,7 +12,7 @@ def main(dataset_func = BTC_1d_Dataset, population=200, generations=200, zscore=
     pop = population
     gen = generations
     
-    name = f'BTC_{pop}p_{gen}g_rules'
+    name = f'BTC_{pop}p_{gen}g_rules_elitism'
     path = 'models/' + name + '.pkl'
 
     df, df_normalized = dataset_func(zscore=zscore)
@@ -37,7 +37,7 @@ def main(dataset_func = BTC_1d_Dataset, population=200, generations=200, zscore=
                                tournament_size=20, rules_set=rules, 
                                indicators_set=indicators, 
                                n_jobs=-1, verbose=verbose, 
-                               random_state=1, elitism=False)
+                               random_state=1, elitism=True)
 
     gp.fit(dataset, prices, 100)
     
@@ -57,4 +57,4 @@ def main(dataset_func = BTC_1d_Dataset, population=200, generations=200, zscore=
     
     
 if __name__ == '__main__':
-    main(dataset_func = BTC_1d_Dataset, population=500, generations=500, zscore=False, verbose=0, load=False, save=True)
+    main(dataset_func = BTC_1d_Dataset, population=500, generations=500, zscore=False, verbose=1, load=False, save=True)
